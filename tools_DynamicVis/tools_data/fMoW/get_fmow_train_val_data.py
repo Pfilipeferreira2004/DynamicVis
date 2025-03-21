1c0289725014a0f975a3b15ab19d97fb545ca756
chenkeyan <chenkeyan@xiaohongshu.com>
2025-03-21 16:00:54 +0800

    add

--- tools_DynamicVis/tools_data/fMoW/get_fmow_train_val_data.py
+++ tools_DynamicVis/tools_data/fMoW/get_fmow_train_val_data.py
@@ -169,7 +169,9 @@ if __name__ == '__main__':
 	# get train, val, test, seq pairs list
 	get_train_val_pairs(data_root, meta_data_save_dir)
 
-	input("Press Enter to continue...")
+	key = input("Press Enter to continue...")
+	if key == 'q':
+		exit()
 	# save as tar
 	for train_test_file in ['pretrain_list.txt', 'test_2w_list.txt']:
 		out_dir_tmp = f'{tar_save_dir}/{os.path.splitext(train_test_file)[0]}'
